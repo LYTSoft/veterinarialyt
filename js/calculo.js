45ac02ff60e2028acb4a5c769fade7bffc68bbdf
yohan <tiara12p@icloud.com>
2024-06-20 14:35:45 -0400

    servicios 3

--- js/calculo.js
+++ js/calculo.js
@@ -1,22 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Espera a que se cargue completamente el DOM antes de ejecutar el código
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    const totalPriceElement = document.getElementById('totalPrice');
-    let totalPrice = 0;
+    const totalElementos = document.getElementById('totalPrecio');
+    let totalPrecios = 0;
 
     checkboxes.forEach(function(checkbox) {
         // Para cada checkbox encontrado en el documento
         checkbox.addEventListener('change', function() {
             // Añade un event listener para el evento 'change' (cambio)
-            const price = parseInt(this.value); // Obtiene el valor numérico del checkbox
+            const precio = parseInt(this.value); // Obtiene el valor numérico del checkbox
             if (this.checked) {
                 // Si el checkbox está marcado
-                totalPrice += price; // Añade el precio al precio total
+                totalPrecios += precio; // Añade el precio al precio total
             } else {
                 // Si el checkbox está desmarcado
-                totalPrice -= price; // Resta el precio del precio total
+                totalPrecios -= precio; // Resta el precio del precio total
             }
-            totalPriceElement.textContent = totalPrice; // Actualiza el elemento HTML con el precio total
+            totalElementos.textContent = totalPrecios; // Actualiza el elemento  con el precio total
         });
+
     });
 });
+
+
+
